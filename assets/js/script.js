7ce44e6f7ef73a997a3ece888da0e686d11410c7
elissamaine <emaine97@yahoo.com>
2023-03-06 15:56:24 -0800

    added ids to html elements that will hold the current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,11 @@ var savedCitiesEl = document.getElementById("past-searches");
 
 var searchedCities = [];
 
+var todaysDate = dayjs().format('MMM D, YYYY')
+
 //var sampleAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=Portland&limit=2&appid=9f46b29f8f812614f70bace845940332'
 //var weatherAPI = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=9f46b29f8f812614f70bace845940332'
+
 
 function searchCity() {
   getLatLon();
@@ -15,6 +18,8 @@ function searchCity() {
 function getLatLon(city) {
   inputCity = inputCity.value.trim();
   console.log(inputCity);
+
+  
 
   var geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputCity + '&limit=1&appid=9f46b29f8f812614f70bace845940332'
   console.log(geocodingAPI)
@@ -28,7 +33,6 @@ function getLatLon(city) {
       currentWeather(data[0].lat, data[0].lon);
       console.log(data);
     })
-    
 }
 
 
@@ -50,32 +54,36 @@ function currentWeather(lat, lon) {
 
 function displayWeather(data) {
   console.log(data);
+  currentWeatherEl.setAttribute('class', 'show');
   
-  var cityName = document.createElement('h2');
-  cityName.textContent = data.city.name;
+  // these will display
+  var cityName = document.getElementById('city-name');
+  cityName.innerHTML = data.city.name;
   console.log(cityName);
 
-  var date = document.createElement('h3');
-  date.textContent = data.list.dt;
+  var date = document.getElementById('date');
+  date.innerHTML = todaysDate;
   console.log(date);
 
-  var weatherIcon = document.createElement('h3');
-  weatherIcon.textContent = data.weather.icon;
-  console.log(weatherIcon);
+ // var weatherIcon = document.getElementById('weather-icon');
+ // weatherIcon.setAttribute();
+  //console.log(weatherIcon);
 
-  var temp = document.createElement('h1');
-  temp.textContent = data.list.main.temp;
+  var temp = document.getElementById('temp');
+  temp.innerHTML = data.list[0].main.temp;
   console.log(temp);
 
-  var humidity = document.createElement('h3');
-  humidity.textContent = data.list.main.humidity;
+  var humidity = document.getElementById('humidity');
+  humidity.innerHTML = data.list[0].main.humidity;
   console.log(humidity);
 
-  var windSpeed = document.createElement('h3');
-  windSpeed.textContent = data.list.wind.speed;
+  var windSpeed = document.getElementById('wind-speed');
+  windSpeed.innerHTML = data.list[0].wind.speed;
   console.log(windSpeed);
-
+ 
   
+
 }
+
 
 document.getElementById('search-button').addEventListener('click', searchCity);
